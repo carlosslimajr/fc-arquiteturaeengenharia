0f137ad4e01b71b0277c26e7329463585672e6ca
Carlosslimajr <carlosslimajrest@gmail.com>
2021-11-19 14:47:12 -0300

    quick head fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,39 +2,30 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Header from 'components/Header'
 import Footer from 'components/Footer'
 import { AppProps } from 'next/app'
-import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from 'styles/global'
 import { theme } from 'themes/primary'
 import Video from 'components/Video'
-import useMediaQuery from 'hooks/useMediaQuery'
 import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 function App({ Component, pageProps }: AppProps) {
-  const mobile = useMediaQuery('(max-width: 991px)')
   const { pathname } = useRouter()
   return (
     <>
       <ThemeProvider theme={theme}>
         <Head>
-          <title>FC Arquitetura e Engenharia</title>
           <link rel="shortcut icon" href="/short4.png" />
-          <link rel="apple-touch-icon" href="/img/icon-512.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#06092B" />
-          <meta name="description" content="FC - Arquitetura e engenharia" />
         </Head>
         <GlobalStyles />
 
         {pathname === '/' && <Video />}
-
         {pathname === '/' && (
           <>
             <div className="background" />
             <div className="background2" />
           </>
         )}
-
         <Header />
         <Component {...pageProps} />
         <Footer />
--- src/pages/briefing.tsx
+++ src/pages/briefing.tsx
@@ -1,10 +1,16 @@
 import Briefing from 'components/Briefing'
-// import Form from 'components/Formulario'
+import SeoComponent from 'components/SeoComponent'
 import React from 'react'
 
 const Brief = () => {
   return (
     <main>
+      <SeoComponent
+        title="Briefing | FC Arquitetura e Engenharia"
+        desc="Escritorio de arquitetura e engenharia com diversas soluções residenciais, comerciais e industriais."
+        url="https://www.fcarquiteturaeengenharia.com.br/"
+        keywords="FC arquitetura e engenharia decoração reforma obra projeto feira de santana fsa salvador construção casa interiores"
+      />
       <Briefing />
     </main>
   )
