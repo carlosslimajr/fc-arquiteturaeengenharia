262e8087aeb755823a9298898cce44dbd4c059c5
Carlosslimajr <carlosslimajrest@gmail.com>
2021-12-13 15:45:22 -0300

    pwa added ?

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 // next.config.js
+/* eslint-disable @typescript-eslint/no-var-requires */
 const withImages = require('next-images')
+const nextPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+const withPlugins = require('next-compose-plugins')
+
+const withPWA = nextPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    runtimeCaching
+  }
+})
+
 module.exports = withImages({
   esModule: true,
   images: {
     disableStaticImages: true
-  }
+  },
+  withPWA
 })
